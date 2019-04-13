7c6eeb6d145831101fe5c155abea6e99ab22692f
fredthedeveloper <fredrickamau18@gmail.com>
2019-04-13 12:41:22 +0300

    changed default route

--- src/EmployeeCRUD/App_Start/RouteConfig.cs
+++ src/EmployeeCRUD/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EmployeeCRUD
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "employees_tbl", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
